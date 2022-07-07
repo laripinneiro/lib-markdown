94bdf03bba1d3908a9f16e843b0e86c305a1c086
Larissa Piñeiro <laripinneiro@gmail.com>
2022-07-07 11:55:50 -0300

    Get links from markdown file using regular expressions

--- index.js
+++ index.js
@@ -9,10 +9,22 @@ async function getFile(filepath) {
 	const encoding = "utf-8";
 	try {
 		const text = await fs.promises.readFile(filepath, encoding);
-		console.log(chalk.green(text));
+		console.log(getLinks(text));
 	} catch (error) {
 		treatError(error);
 	}
+}
+
+function getLinks(text) {
+	const regex = /\[([^\]]*)\]\((https?:\/\/[^$#\s].[^\s]*)\)/gm;
+	const textLinks = [];
+	let temp;
+
+	while ((temp = regex.exec(text)) !== null) {
+		textLinks.push({ [temp[1]]: temp[2] });
+	}
+
+	return textLinks;
 }
 
 getFile("assets/text1.md");
